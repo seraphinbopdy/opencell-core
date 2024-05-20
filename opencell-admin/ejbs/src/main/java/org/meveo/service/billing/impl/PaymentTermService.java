e7a5d77620d259b9c87cbb15f940b5a1b63fba05
tarik.fakhouri <tarik.fakhouri@opencellsoft.com>
2024-05-20 13:48:38 +0100

    INTRD-23050 [back] Create paymentTerm APIs

---
+++ opencell-admin/ejbs/src/main/java/org/meveo/service/billing/impl/PaymentTermService.java
@@ -0,0 +1,19 @@
+package org.meveo.service.billing.impl;
+
+import org.meveo.model.payments.PaymentTerm;
+import org.meveo.service.base.PersistenceService;
+
+import javax.persistence.NoResultException;
+
+public class PaymentTermService extends PersistenceService<PaymentTerm> {
+	
+	public PaymentTerm findByCode(String paymentTermCode) {
+		try{
+			return getEntityManager().createNamedQuery("PaymentTerm.findByCode", PaymentTerm.class)
+					.setParameter("code", paymentTermCode.toLowerCase())
+					.getSingleResult();
+		}catch (NoResultException e){
+			return null;
+		}
+	}
+}

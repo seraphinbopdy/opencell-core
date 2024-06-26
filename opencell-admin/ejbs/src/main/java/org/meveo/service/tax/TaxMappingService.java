2222a234c02355eba2fb44e3837031896ec62f8c
tarik.fakhouri <tarik.fakhouri@opencellsoft.com>
2024-06-26 10:17:17 +0100

    INTRD-24048 [Reg | API] Unable to activate the service instances

--- opencell-admin/ejbs/src/main/java/org/meveo/service/tax/TaxMappingService.java
+++ opencell-admin/ejbs/src/main/java/org/meveo/service/tax/TaxMappingService.java
@@ -336,7 +336,13 @@ public class TaxMappingService extends PersistenceService<TaxMapping> {
 	
 	private void setTaxForWallet(TaxInfo taxInfo, WalletOperation walletOperation) {
 		if(taxInfo != null){
-			walletOperation.setTaxClass(getEntityManager().getReference(TaxClass.class, taxInfo.taxClass.getId()));
+			if(taxInfo.taxClass != null){
+				if(taxInfo.taxClass instanceof TaxClass){
+					walletOperation.setTaxClass(taxInfo.taxClass);
+				}else{
+					walletOperation.setTaxClass((TaxClass) ((HibernateProxy) taxInfo.taxClass).getHibernateLazyInitializer().getImplementation());
+				}
+			}
 			walletOperation.setTax(taxInfo.tax);
 			walletOperation.setTaxPercent(taxInfo.tax.getPercent());
 		}

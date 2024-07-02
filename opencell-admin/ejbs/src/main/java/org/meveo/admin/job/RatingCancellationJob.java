bea60510a33da1e2935806c4bf086f771102c3bf
Anas ROUAGUEBE <anas.rouaguebe@opencellsoft.com>
2024-07-02 08:08:37 +0100

    INTRD-24052 - ReRating V2 - Add filter to RatingCancellationJob

--- opencell-admin/ejbs/src/main/java/org/meveo/admin/job/RatingCancellationJob.java
+++ opencell-admin/ejbs/src/main/java/org/meveo/admin/job/RatingCancellationJob.java
@@ -8,7 +8,10 @@ import javax.inject.Inject;
 
 import org.meveo.admin.exception.BusinessException;
 import org.meveo.admin.job.utils.CustomFieldTemplateUtils;
+import org.meveo.model.billing.BatchEntity;
+import org.meveo.model.billing.ReratingTargetEnum;
 import org.meveo.model.crm.CustomFieldTemplate;
+import org.meveo.model.crm.custom.CustomFieldStorageTypeEnum;
 import org.meveo.model.crm.custom.CustomFieldTypeEnum;
 import org.meveo.model.jobs.JobCategoryEnum;
 import org.meveo.model.jobs.JobExecutionResultImpl;
@@ -24,6 +27,16 @@ public class RatingCancellationJob extends Job {
 	public static final String CF_INVOICE_LINES_NR_RTS_PER_TX = "JobInstance_RatingCancellationJob_MaxRTsPerTransaction";
 
 	public static final String CF_USE_EXISTING_VIEWS = "JobInstance_RatingCancellationJob_UseExistingViews";
+
+    /**
+     * To limit the scope of wallet operations to rerate.
+     */
+    public static final String CF_RERATING_TARGET = "ReRatingJobBean_reratingTarget";
+
+    /**
+     * Custom field contains a list of batch entities
+     */
+    public static final String CF_TARGET_BATCHES = "ReRatingJobBean_targetBatches";
 
     @Inject
     private RatingCancellationJobBean jobBean;
@@ -51,6 +64,27 @@ public class RatingCancellationJob extends Job {
                 "tab:Configuration:0;fieldGroup:Configuration:0;field:2", "100000", true, JOB_INSTANCE_RATING_CANCELLATION_JOB));
         result.put(CF_USE_EXISTING_VIEWS, CustomFieldTemplateUtils.buildCF(CF_USE_EXISTING_VIEWS, resourceMessages.getString("jobExecution.useExistingViews"), CustomFieldTypeEnum.BOOLEAN,
                 "tab:Configuration:0;fieldGroup:Configuration:0;field:3", "true", JOB_INSTANCE_RATING_CANCELLATION_JOB));
+
+        CustomFieldTemplate reratingTargetCFTemplate = CustomFieldTemplateUtils.buildCF(CF_RERATING_TARGET,
+                resourceMessages.getString("jobExecution.reratingTarget"), CustomFieldTypeEnum.LIST,
+                "tab:Configuration:0;fieldGroup:Configuration:0;field:4",
+                ReratingTargetEnum.ALL.name(), false, CustomFieldStorageTypeEnum.SINGLE, null,
+                JOB_INSTANCE_RATING_CANCELLATION_JOB, null);
+        Map<String, String> listValues = new HashMap();
+        for (ReratingTargetEnum reratingTarget : ReratingTargetEnum.values()) {
+            listValues.put(reratingTarget.name(), reratingTarget.getLabel());
+        }
+        reratingTargetCFTemplate.setListValues(listValues);
+        result.put(CF_RERATING_TARGET, reratingTargetCFTemplate);
+
+
+        CustomFieldTemplate targetBatchesCFTemplate = CustomFieldTemplateUtils.buildCF(CF_TARGET_BATCHES,
+                resourceMessages.getString("jobExecution.targetBatches"), CustomFieldTypeEnum.ENTITY,
+                "tab:Configuration:0;fieldGroup:Configuration:0;field:5",
+                null, false, CustomFieldStorageTypeEnum.LIST, null,
+                JOB_INSTANCE_RATING_CANCELLATION_JOB, null);
+        targetBatchesCFTemplate.setEntityClazz(BatchEntity.class.getName());
+        result.put(CF_TARGET_BATCHES, targetBatchesCFTemplate);
 
         return result;
     }

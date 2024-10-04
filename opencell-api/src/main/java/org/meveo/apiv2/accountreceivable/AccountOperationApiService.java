e47a7b9d83d21d2ba1c1402d1e5ba26708bf0bac
Abdelmounaim Akadid <abdelmounaim.akadid@opencellsoft.com>
2024-10-04 10:00:52 +0100

    INTRD-25698 revert

--- opencell-api/src/main/java/org/meveo/apiv2/accountreceivable/AccountOperationApiService.java
+++ opencell-api/src/main/java/org/meveo/apiv2/accountreceivable/AccountOperationApiService.java
@@ -199,7 +199,7 @@ public class AccountOperationApiService implements ApiService<AccountOperation> 
 		// Check existence of all passed accountOperation
 		List<AccountOperation> aos = new ArrayList<>();
 		aoIds.forEach(aoId -> aos.add(accountOperationService.findById(aoId)));
-		checkIncompatibleAccountOperationTypes(aos);
+
 		if (aoIds.size() != aos.size()) {
 			throw new EntityDoesNotExistsException("One or more AccountOperations passed for matching are not found");
 		}
@@ -594,30 +594,4 @@ public class AccountOperationApiService implements ApiService<AccountOperation> 
 		
 	}
 	
-	private void checkIncompatibleAccountOperationTypes(List<AccountOperation> accountOperations) {
-			if (accountOperations.size() <= 1) {
-				return;
-			}
-			Map<String, Set<String>> validMatches = Map.of(
-					"INV_ADV", Set.of("PAY_ADV", "CLOSED_ADV"),
-					"PAY_ADV", Set.of("INV_ADV", "REJ_ADV"),
-					"REJ_ADV", Set.of("PAY_ADV"),
-					"CLOSED_ADV", Set.of("INV_ADV")
-			);
-			
-			String firstCode = accountOperations.get(0).getCode();
-			Set<String> validCodes = validMatches.get(firstCode);
-			
-			if (validCodes == null) {
-				return;
-			}
-			
-			boolean allMatch = accountOperations.stream()
-					.skip(1)
-					.allMatch(ao -> validCodes.contains(ao.getCode()));
-			
-			if (!allMatch) {
-				throw new BusinessException("Incompatible account operation types. " + firstCode + " account operations can only be matched with " + validCodes + "  account operations");
-			}
-		}
 }

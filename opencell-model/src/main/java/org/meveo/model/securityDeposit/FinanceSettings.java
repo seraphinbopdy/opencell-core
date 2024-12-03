29489881c0c84f33096ec6cdc94c5831de3d4a90
Andrius Karpavicius <andrius.karpavicius@opencellsoft.com>
2024-12-04 01:31:01 +0200

    Re #INTRD-27609 Added caching to FinanceSettings and hashCode() and equals() to HugeEntity class

--- opencell-model/src/main/java/org/meveo/model/securityDeposit/FinanceSettings.java
+++ opencell-model/src/main/java/org/meveo/model/securityDeposit/FinanceSettings.java
@@ -11,6 +11,7 @@ import org.hibernate.type.SqlTypes;
 import org.meveo.model.BusinessEntity;
 import org.meveo.model.settings.OpenOrderSetting;
 
+import jakarta.persistence.Cacheable;
 import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Convert;
@@ -25,6 +26,7 @@ import jakarta.persistence.Table;
 import jakarta.validation.constraints.Digits;
 
 @Entity
+@Cacheable
 @Table(name = "finance_settings")
 @GenericGenerator(name = "ID_GENERATOR", type = org.hibernate.id.enhanced.SequenceStyleGenerator.class, parameters = { @Parameter(name = "sequence_name", value = "finance_settings_seq"), @Parameter(name = "increment_size", value = "1") })
 public class FinanceSettings extends BusinessEntity {

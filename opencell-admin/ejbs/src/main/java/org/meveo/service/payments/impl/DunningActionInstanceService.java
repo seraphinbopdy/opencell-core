f875b17c46b4a91d60e69af65b91c1151e295a4e
Zakaria EL MELIANI <z.meliani_ext@pixid.com>
2025-01-20 16:48:12 +0100

    INTRD-28654: Fix template when sending email in dunning invoice level mode

--- opencell-admin/ejbs/src/main/java/org/meveo/service/payments/impl/DunningActionInstanceService.java
+++ opencell-admin/ejbs/src/main/java/org/meveo/service/payments/impl/DunningActionInstanceService.java
@@ -8,6 +8,7 @@ import java.util.*;
 import jakarta.inject.Inject;
 import org.meveo.admin.exception.BusinessException;
 import org.meveo.commons.utils.QueryBuilder;
+import org.meveo.commons.utils.StringUtils;
 import org.meveo.model.admin.Seller;
 import org.meveo.model.billing.BillingAccount;
 import org.meveo.model.billing.Invoice;
@@ -190,7 +191,7 @@ public class DunningActionInstanceService extends PersistenceService<DunningActi
      */
     private void executeNotificationAction(DunningSettings dunningSettings, DunningActionInstance actionInstance, Invoice pInvoice, DunningCollectionPlan collectionPlan) {
         if(dunningSettings != null && dunningSettings.getDunningMode() == DunningModeEnum.INVOICE_LEVEL) {
-            sendEmail(actionInstance.getDunningAction().getActionNotificationTemplate(), collectionPlan != null && collectionPlan.getRelatedInvoice() != null ? collectionPlan.getRelatedInvoice() : pInvoice, collectionPlan != null && collectionPlan.getLastActionDate() != null ? collectionPlan.getLastActionDate() : null);
+            sendEmail(actionInstance.getDunningAction().getActionNotificationTemplate(), collectionPlan, pInvoice);
         } else if (dunningSettings != null && dunningSettings.getDunningMode() == DunningModeEnum.CUSTOMER_LEVEL) {
             customerAccountService.sendEmail(actionInstance.getDunningAction().getActionNotificationTemplate(), actionInstance.getCollectionPlan());
         }
@@ -199,78 +200,110 @@ public class DunningActionInstanceService extends PersistenceService<DunningActi
     /**
      * Send email
      *
-     * @param emailTemplate Email template
-     * @param invoice Invoice
-     * @param lastActionDate Last action date
+     * @param pEmailTemplate Email template
+     * @param pCollectionPlan Collection Plan
+     * @param pInvoice Invoice
      */
-    private void sendEmail(EmailTemplate emailTemplate, Invoice invoice, Date lastActionDate) {
+    private void sendEmail(EmailTemplate pEmailTemplate, DunningCollectionPlan pCollectionPlan, Invoice pInvoice) {
+        // Get the invoice from collection plan
+        Invoice invoice = pCollectionPlan != null && pCollectionPlan.getRelatedInvoice() != null ? pCollectionPlan.getRelatedInvoice() : pInvoice;
+
         if (invoice.getSeller() != null
                 && invoice.getSeller().getContactInformation() != null
                 && invoice.getSeller().getContactInformation().getEmail() != null
                 && !invoice.getSeller().getContactInformation().getEmail().isBlank()) {
+            // Get Billing Account and Customer Account
+            BillingAccount billingAccount = billingAccountService.findById(invoice.getBillingAccount().getId(), List.of("customerAccount"));
+            CustomerAccount customerAccount = customerAccountService.findById(billingAccount.getCustomerAccount().getId());
             // prepare params
             Map<Object, Object> params = new HashMap<>();
-
-            // get billing account infos and set it into params
-            BillingAccount billingAccount = billingAccountService.findById(invoice.getBillingAccount().getId(), List.of("customerAccount"));
-            setBillingAccountInfo(params, billingAccount);
-
-            // get customer account infos and set it into params
-            CustomerAccount customerAccount = customerAccountService.findById(billingAccount.getCustomerAccount().getId());
-            setCustomerAccountInfo(params, customerAccount);
-
-            // set invoice infos
-            params.put("invoiceInvoiceNumber", invoice.getInvoiceNumber());
-            params.put("invoiceTotal", invoice.getAmountWithTax());
-            params.put("invoiceDueDate", formatter.format(invoice.getDueDate()));
-            params.put("dayDate", formatter.format(new Date()));
-            params.put("dunningCollectionPlanLastActionDate", lastActionDate != null ? formatter.format(lastActionDate) : "");
-
+            // Set billing account, Customer Account, Invoice and Collection Plan information's
+            setBillingAccountInfos(params, billingAccount);
+            setCustomerAccountInfos(params, customerAccount);
+            setCollectionPlanInfos(params, pCollectionPlan);
+            setInvoiceInfos(params, invoice);
             // prepare attachments
             List<File> attachments = setAttachment(invoice);
 
             // send notification
-            sendNotification(billingAccount, invoice.getSeller(), emailTemplate, params, attachments);
+            sendNotification(billingAccount, invoice.getSeller(), pEmailTemplate, params, attachments);
         } else {
             throw new BusinessException("The email sending skipped because the from email is missing for the seller : " + invoice.getSeller().getCode());
         }
     }
 
     /**
-     * Set billing account info
+     * Set billing account infos
      *
      * @param params Params
-     * @param billingAccount Billing account
+     * @param billingAccount Billing account {@link BillingAccount}
      */
-    private void setBillingAccountInfo(Map<Object, Object> params, BillingAccount billingAccount) {
+    private void setBillingAccountInfos(Map<Object, Object> params, BillingAccount billingAccount) {
         params.put("billingAccountDescription", billingAccount.getDescription());
-        params.put("billingAccountAddressAddress1", billingAccount.getAddress() != null ? billingAccount.getAddress().getAddress1() : "");
-        params.put("billingAccountAddressZipCode", billingAccount.getAddress() != null ? billingAccount.getAddress().getZipCode() : "");
-        params.put("billingAccountAddressCity", billingAccount.getAddress() != null ? billingAccount.getAddress().getCity() : "");
-        params.put("billingAccountContactInformationPhone", billingAccount.getContactInformation() != null ? billingAccount.getContactInformation().getPhone() : "");
+        params.put("billingAccountAddressAddress1", billingAccount.getAddress() != null ? billingAccount.getAddress().getAddress1() : StringUtils.EMPTY);
+        params.put("billingAccountAddressZipCode", billingAccount.getAddress() != null ? billingAccount.getAddress().getZipCode() : StringUtils.EMPTY);
+        params.put("billingAccountAddressCity", billingAccount.getAddress() != null ? billingAccount.getAddress().getCity() : StringUtils.EMPTY);
+        params.put("billingAccountContactInformationPhone", billingAccount.getContactInformation() != null ? billingAccount.getContactInformation().getPhone() : StringUtils.EMPTY);
+        params.put("billingAccountLegalEntityTypeCode", billingAccount.getLegalEntityType() != null ? billingAccount.getLegalEntityType().getCode() : StringUtils.EMPTY);
+        params.put("billingAccountCcedEmails", billingAccount.getCcedEmails());
+        params.put("contactInformationEmail", billingAccount.getContactInformation() != null ? billingAccount.getContactInformation().getEmail() : StringUtils.EMPTY);
+        params.put("contactInformationPhone", billingAccount.getContactInformation() != null ? billingAccount.getContactInformation().getPhone() : StringUtils.EMPTY);
+        params.put("contactInformationMobile", billingAccount.getContactInformation() != null ? billingAccount.getContactInformation().getMobile() : StringUtils.EMPTY);
 
         if (Boolean.TRUE.equals(billingAccount.getIsCompany())) {
-            params.put("customerAccountLegalEntityTypeCode", ofNullable(billingAccount.getLegalEntityType()).map(Title::getCode).orElse(""));
+            params.put("customerAccountLegalEntityTypeCode", ofNullable(billingAccount.getLegalEntityType()).map(Title::getCode).orElse(StringUtils.EMPTY));
         } else {
             Name name = billingAccount.getName();
             Title title = ofNullable(name).map(Name::getTitle).orElse(null);
-            params.put("customerAccountLegalEntityTypeCode", ofNullable(title).map(Title::getDescription).orElse(""));
+            params.put("customerAccountLegalEntityTypeCode", ofNullable(title).map(Title::getDescription).orElse(StringUtils.EMPTY));
         }
     }
 
     /**
-     * Set customer account info
+     * Set customer account infos
      *
      * @param params Params
-     * @param customerAccount Customer account
+     * @param customerAccount Customer account {@link CustomerAccount}
      */
-    private void setCustomerAccountInfo(Map<Object, Object> params, CustomerAccount customerAccount) {
-        params.put("customerAccountAddressAddress1", customerAccount.getAddress() != null ? customerAccount.getAddress().getAddress1() : "");
-        params.put("customerAccountAddressZipCode", customerAccount.getAddress() != null ? customerAccount.getAddress().getZipCode() : "");
-        params.put("customerAccountAddressCity", customerAccount.getAddress() != null ? customerAccount.getAddress().getCity() : "");
+    private void setCustomerAccountInfos(Map<Object, Object> params, CustomerAccount customerAccount) {
+        params.put("customerAccountAddressAddress1", customerAccount.getAddress() != null ? customerAccount.getAddress().getAddress1() : StringUtils.EMPTY);
+        params.put("customerAccountAddressZipCode", customerAccount.getAddress() != null ? customerAccount.getAddress().getZipCode() : StringUtils.EMPTY);
+        params.put("customerAccountAddressCity", customerAccount.getAddress() != null ? customerAccount.getAddress().getCity() : StringUtils.EMPTY);
         params.put("customerAccountDescription", customerAccount.getDescription());
-        params.put("customerAccountLastName", customerAccount.getName() != null ? customerAccount.getName().getLastName() : "");
-        params.put("customerAccountFirstName", customerAccount.getName() != null ? customerAccount.getName().getFirstName() : "");
+        params.put("customerAccountLastName", customerAccount.getName() != null ? customerAccount.getName().getLastName() : StringUtils.EMPTY);
+        params.put("customerAccountFirstName", customerAccount.getName() != null ? customerAccount.getName().getFirstName() : StringUtils.EMPTY);
+    }
+
+    /**
+     * Set invoice infos
+     *
+     * @param params Params
+     * @param invoice Invoice {@link Invoice}
+     */
+    private void setInvoiceInfos(Map<Object, Object> params, Invoice invoice) {
+        params.put("invoiceInvoiceNumber", invoice.getInvoiceNumber());
+        params.put("invoiceOrderOrderNumber", invoice.getOpenOrderNumber());
+        params.put("invoicePaymentMethodType", invoice.getPaymentMethod() != null ? invoice.getPaymentMethod().getPaymentType().name() : StringUtils.EMPTY);
+        params.put("invoiceAmountWithoutTax", invoice.getAmountWithoutTax());
+        params.put("invoiceAmountWithTax", invoice.getAmountWithTax());
+        params.put("invoicePaymentStatus", invoice.getPaymentStatus().name());
+        params.put("invoiceTotal", invoice.getAmountWithTax());
+        params.put("invoiceDueDate", formatter.format(invoice.getDueDate()));
+        params.put("dayDate", formatter.format(new Date()));
+    }
+
+    /**
+     * Set collection plan infos
+     *
+     * @param params Params
+     * @param pCollectionPlan Collection Plan {@link DunningCollectionPlan}
+     */
+    private void setCollectionPlanInfos(Map<Object, Object> params, DunningCollectionPlan pCollectionPlan) {
+        params.put("dunningCollectionPlanId", pCollectionPlan != null ? pCollectionPlan.getCollectionPlanNumber() : StringUtils.EMPTY);
+        params.put("dunningCollectionPlanStatusStatus", pCollectionPlan != null ? pCollectionPlan.getStatus().getStatus().name() : StringUtils.EMPTY);
+        params.put("dunningCollectionPlanLastAction", pCollectionPlan != null ? pCollectionPlan.getLastAction() : StringUtils.EMPTY);
+        String lastActionDate = pCollectionPlan != null && pCollectionPlan.getLastActionDate() != null ? formatter.format(pCollectionPlan.getLastActionDate()) : null;
+        params.put("dunningCollectionPlanLastActionDate", lastActionDate != null ? formatter.format(lastActionDate) : StringUtils.EMPTY);
     }
 
     /**

d0bca2f2b980b13e6c6747ca8a32348d26905a84
Anas ROUAGUEBE <anas.rouaguebe@opencellsoft.com>
2025-01-22 11:21:47 +0100

    INTRD-28777 - Language - add missing lang + fix error codes

--- opencell-api/src/main/java/org/meveo/apiv2/language/impl/LanguageResourceImpl.java
+++ opencell-api/src/main/java/org/meveo/apiv2/language/impl/LanguageResourceImpl.java
@@ -5,7 +5,9 @@ import jakarta.interceptor.Interceptors;
 import jakarta.ws.rs.core.Response;
 import org.meveo.api.dto.ActionStatus;
 import org.meveo.api.dto.ActionStatusEnum;
+import org.meveo.api.exception.MeveoApiException;
 import org.meveo.api.logging.WsRestApiInterceptor;
+import org.meveo.api.rest.impl.BaseRs;
 import org.meveo.apiv2.language.LanguageDto;
 import org.meveo.apiv2.language.resource.LanguageResource;
 import org.meveo.apiv2.language.service.LanguageApiService;
@@ -13,16 +15,13 @@ import org.meveo.model.billing.Language;
 
 
 @Interceptors({ WsRestApiInterceptor.class })
-public class LanguageResourceImpl implements LanguageResource {
+public class LanguageResourceImpl extends BaseRs implements LanguageResource {
 
     @Inject
     private LanguageApiService languageApiService;
 
     @Override
     public Response createLanguage(LanguageDto languageDto) {
-        if (languageDto.getCode() == null || languageDto.getDescription() == null) {
-            return Response.status(412).entity("Missing parameters: 'code' and 'description' are required").build();
-        }
 
         try {
             Language language =  languageApiService.createLanguage(languageDto);
@@ -33,21 +32,14 @@ public class LanguageResourceImpl implements LanguageResource {
             responseStatus.setEntityCode(languageDto.getCode());
 
             return Response.ok(responseStatus).build();
-        } catch (Exception e) {
-            return Response.status(400).entity("Language creation failed: " + e.getMessage()).build();
+        } catch (MeveoApiException e) {
+            return errorResponse(e, new ActionStatus());
         }
     }
 
 
     @Override
     public Response updateLanguage(Long id, LanguageDto languageDto) {
-        if (id == null) {
-            return Response.status(412).entity("Missing ID").build();
-        }
-        if (languageDto == null) {
-            return Response.status(412).entity("Missing request body").build();
-        }
-
         try {
             languageApiService.updateLanguage(id, languageDto);
 
@@ -55,8 +47,8 @@ public class LanguageResourceImpl implements LanguageResource {
             responseStatus.setStatus(ActionStatusEnum.SUCCESS);
 
             return Response.ok(responseStatus).build();
-        } catch (Exception e) {
-            return Response.status(400).entity("Language creation failed: " + e.getMessage()).build();
+        } catch (MeveoApiException e) {
+            return errorResponse(e, new ActionStatus());
         }
     }
 }
--- opencell-api/src/main/java/org/meveo/apiv2/language/service/LanguageApiService.java
+++ opencell-api/src/main/java/org/meveo/apiv2/language/service/LanguageApiService.java
@@ -2,13 +2,18 @@ package org.meveo.apiv2.language.service;
 
 import jakarta.inject.Inject;
 
+import jakarta.ws.rs.core.Response;
 import org.meveo.admin.exception.BusinessException;
 import org.meveo.api.exception.EntityAlreadyExistsException;
 import org.meveo.api.exception.EntityDoesNotExistsException;
+import org.meveo.api.exception.MissingParameterException;
 import org.meveo.apiv2.language.LanguageDto;
 import org.meveo.apiv2.language.mapper.LanguageMapper;
 import org.meveo.model.billing.Language;
 import org.meveo.service.admin.impl.LanguageService;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class LanguageApiService {
 
@@ -18,6 +23,16 @@ public class LanguageApiService {
     LanguageMapper mapper = new LanguageMapper();
 
     public Language createLanguage(LanguageDto languageDto) {
+        List<String> missingParameters = new ArrayList<>();
+        if(languageDto.getCode() == null) {
+            missingParameters.add("code");
+        }
+        if(languageDto.getDescription() == null) {
+            missingParameters.add("description");
+        }
+        if(!missingParameters.isEmpty()) {
+            throw new MissingParameterException(missingParameters);
+        }
         
         if(languageService.findByCode(languageDto.getCode()) != null) {
             throw new EntityAlreadyExistsException(Language.class, languageDto.getCode());
@@ -37,6 +52,10 @@ public class LanguageApiService {
         Language existingLanguage = languageService.findById(id);
         if(existingLanguage == null) {
             throw new EntityDoesNotExistsException(Language.class, id);
+        }
+
+        if (id == null) {
+            throw new MissingParameterException("id");
         }
         
         Language entity = mapper.toEntity(existingLanguage, languageDto);

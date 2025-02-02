405d4ad1248ef02e14aab0784b5d8189730341e9
Anas ROUAGUEBE <anas.rouaguebe@opencellsoft.com>
2025-02-02 23:51:50 +0100

    INTRD-28863 - Fix create OneShot using script

--- opencell-admin/ejbs/src/main/java/org/meveo/service/billing/impl/InvoiceService.java
+++ opencell-admin/ejbs/src/main/java/org/meveo/service/billing/impl/InvoiceService.java
@@ -6370,7 +6370,6 @@ public class InvoiceService extends PersistenceService<Invoice> {
 
                     if (!ilMassUpdates.isEmpty() || !ilUpdates.isEmpty()) {
                         em.flush();
-                        em.clear();
                     }
                 }
             }

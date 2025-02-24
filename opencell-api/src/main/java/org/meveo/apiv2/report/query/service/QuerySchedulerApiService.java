6211d39c2c3a57447e99f05e1ad70783cd609dbc
Abdelkader Bouazza <abdelkader.bouazza@opencellsoft.com>
2025-02-24 10:35:19 +0100

    INTRD-2927: create and attach a TimerEntity to the query job, when creating a QueryScheduler

--- opencell-api/src/main/java/org/meveo/apiv2/report/query/service/QuerySchedulerApiService.java
+++ opencell-api/src/main/java/org/meveo/apiv2/report/query/service/QuerySchedulerApiService.java
@@ -7,27 +7,35 @@ import java.util.Collections;
 import java.util.List;
 import java.util.Optional;
 
+import jakarta.ejb.ScheduleExpression;
 import jakarta.inject.Inject;
 import jakarta.transaction.Transactional;
 import jakarta.ws.rs.BadRequestException;
 import jakarta.ws.rs.NotFoundException;
 
 import org.meveo.apiv2.ordering.services.ApiService;
+import org.meveo.commons.utils.StringUtils;
 import org.meveo.model.admin.User;
 import org.meveo.model.jobs.JobInstance;
 import org.meveo.model.jobs.MeveoJobCategoryEnum;
+import org.meveo.model.jobs.TimerEntity;
 import org.meveo.model.report.query.QueryScheduler;
+import org.meveo.model.report.query.QueryTimer;
 import org.meveo.model.report.query.ReportQuery;
 import org.meveo.security.CurrentUser;
 import org.meveo.security.MeveoUser;
 import org.meveo.service.admin.impl.UserService;
 import org.meveo.service.job.JobInstanceService;
+import org.meveo.service.job.TimerEntityService;
 import org.meveo.service.report.QuerySchedulerService;
 
 public class QuerySchedulerApiService implements ApiService<QueryScheduler> {
 
     @Inject
     private QuerySchedulerService querySchedulerService;
+
+	@Inject
+	private TimerEntityService timerEntityService;
     
     @Inject
     private JobInstanceService jobInstanceService;
@@ -41,51 +49,131 @@ public class QuerySchedulerApiService implements ApiService<QueryScheduler> {
 
 	@Transactional
     @Override
-    public QueryScheduler create(QueryScheduler entity) {
+    public QueryScheduler create(QueryScheduler inputQueryScheduler) {
         try {
-        	List<User> usersToNotify = new ArrayList<>();
-        	for(User element: entity.getUsersToNotify()) {
-        		User user = userService.findByUsername(element.getUserName(), false, false);
-        		if(user == null && element.getId() != null) {
-        			user = userService.findById(element.getId());
-        		}
-        		if(user == null) {
-        			throw new NotFoundException("The user with id {" + element.getId() + "} or userName {" + element.getUserName() + "} does not exists");
-        		}
-        		usersToNotify.add(user);
-        	}
-        	entity.setUsersToNotify(usersToNotify);
+			inputQueryScheduler.setUsersToNotify(getUsersToNotify(inputQueryScheduler));
 
-        	ReportQuery reportQuery = entity.getReportQuery();
+        	ReportQuery reportQuery = inputQueryScheduler.getReportQuery();
         	String code = reportQuery.getCode() + "_Job";
 			JobInstance jobInstance = jobInstanceService.findByCode(code);
-			boolean isDisabledJob = !entity.getIsQueryScheduler();
-			if (jobInstance != null) {
-				entity = jobInstance.getQueryScheduler() != null ? querySchedulerService.findById(jobInstance.getQueryScheduler().getId()) : entity;
-			}else {
+			boolean isDisabledJob = !inputQueryScheduler.getIsQueryScheduler();
+			TimerEntity inputTimer = toTimer(inputQueryScheduler.getQueryTimer());
+			if (jobInstance == null) {
 				jobInstance = new JobInstance();
+				jobInstance.setTimerEntity(inputTimer);
+			}else if(jobInstance.getQueryScheduler() != null){
+				updateExistingQueryScheduler(jobInstance, inputQueryScheduler);
 			}
-
+			updateOrCreateTimer(jobInstance, inputTimer);
 			jobInstance.setCode(code);
-            jobInstance.setDescription("Job for report query='" + reportQuery.getCode() + "'");
-            jobInstance.setJobCategoryEnum(MeveoJobCategoryEnum.REPORTING_QUERY);
-            jobInstance.setJobTemplate("ReportQueryJob");
-            jobInstance.setCfValue("reportQuery", reportQuery);
-            jobInstance.setQueryScheduler(entity);
+			jobInstance.setDescription("Job for report query='" + reportQuery.getCode() + "'");
+			jobInstance.setJobCategoryEnum(MeveoJobCategoryEnum.REPORTING_QUERY);
+			jobInstance.setJobTemplate("ReportQueryJob");
+			jobInstance.setCfValue("reportQuery", reportQuery);
 			jobInstance.setDisabled(isDisabledJob);
 			if(jobInstance.getId() == null) {
-				querySchedulerService.create(entity);
-				entity.setJobInstance(jobInstance);
+				inputQueryScheduler.setJobInstance(jobInstance);
+				querySchedulerService.create(inputQueryScheduler);
 				jobInstanceService.create(jobInstance);
+			}else{
+				jobInstanceService.update(jobInstance);
 			}
-			jobInstanceService.update(jobInstance);
-            return entity;
+            return inputQueryScheduler;
         } catch (Exception exception) {
             throw new BadRequestException(exception.getMessage());
         }
     }
 
+	private void updateOrCreateTimer(JobInstance jobInstance, TimerEntity inputTimer) {
+		if(jobInstance.getTimerEntity() != null && jobInstance.getTimerEntity().getId() != null ){
+			TimerEntity timerEntity = jobInstance.getTimerEntity();
+			timerEntity.setActive(inputTimer.isActive());
+			timerEntity.setCode(inputTimer.getCode());
+			timerEntity.setJobInstances(List.of(jobInstance));
+			timerEntity.setHour(inputTimer.getHour());
+			timerEntity.setMinute(inputTimer.getMinute());
+			timerEntity.setYear(inputTimer.getYear());
+			timerEntity.setSecond(inputTimer.getSecond());
+			timerEntity.setMonth(inputTimer.getMonth());
+			timerEntity.setDayOfMonth(inputTimer.getDayOfMonth());
+			timerEntity.setDayOfWeek(inputTimer.getDayOfWeek());
+			timerEntity.setEnd(inputTimer.getEnd());
+			timerEntity.setStart(inputTimer.getStart());
+			timerEntity.setDescription(inputTimer.getDescription());
+			timerEntity.setDisabled(inputTimer.isDisabled());
+			timerEntityService.update(timerEntity);
+		}else{
+			timerEntityService.create(inputTimer);
+			jobInstance.setTimerEntity(inputTimer);
+		}
+	}
 
+	private void updateExistingQueryScheduler(JobInstance jobInstance, QueryScheduler inputQueryScheduler) {
+		QueryScheduler existingQueryScheduler = jobInstance.getQueryScheduler();
+		if(existingQueryScheduler != null) {
+			existingQueryScheduler.setVersion(existingQueryScheduler.getVersion()+1);
+			existingQueryScheduler.setIsQueryScheduler(inputQueryScheduler.getIsQueryScheduler());
+			existingQueryScheduler.setQueryTimer(inputQueryScheduler.getQueryTimer());
+			existingQueryScheduler.setUsersToNotify(inputQueryScheduler.getUsersToNotify());
+			existingQueryScheduler.setEmailsToNotify(inputQueryScheduler.getEmailsToNotify());
+			existingQueryScheduler.setFileFormat(inputQueryScheduler.getFileFormat());
+			existingQueryScheduler.setIsQueryScheduler(inputQueryScheduler.getIsQueryScheduler());
+		}else {
+			jobInstance.setQueryScheduler(inputQueryScheduler);
+		}
+		querySchedulerService.update(jobInstance.getQueryScheduler());
+	}
+
+	private List<User> getUsersToNotify(QueryScheduler entity) {
+		List<User> usersToNotify = new ArrayList<>();
+		for(User element: entity.getUsersToNotify()) {
+			User user = userService.findByUsername(element.getUserName(), false, false);
+			if(user == null && element.getId() != null) {
+				user = userService.findById(element.getId());
+			}
+			if(user == null) {
+				throw new NotFoundException("The user with id {" + element.getId() + "} or userName {" + element.getUserName() + "} does not exists");
+			}
+			user.getUserRoles().size();
+			usersToNotify.add(user);
+		}
+		return usersToNotify;
+	}
+
+	private TimerEntity toTimer(QueryTimer queryTimer) {
+		TimerEntity timer = new TimerEntity();
+		timer.setDayOfMonth(StringUtils.isBlank(queryTimer.getDayOfMonth()) ? "*" : (queryTimer.isEveryDayOfMonth() ? "*/" + queryTimer.getDayOfMonth() : queryTimer.getDayOfMonth()));
+		timer.setDayOfWeek(StringUtils.isBlank(queryTimer.getDayOfWeek()) ? "*" : (queryTimer.isEveryDayOfWeek() ? "*/" + queryTimer.getDayOfWeek() : queryTimer.getDayOfWeek()));
+		timer.setHour(StringUtils.isBlank(queryTimer.getHour()) ? "*" : (queryTimer.isEveryHour() ? "*/" + queryTimer.getHour() : queryTimer.getHour()));
+		timer.setMinute(StringUtils.isBlank(queryTimer.getMinute()) ? "*" : (queryTimer.isEveryMinute() ? "*/" + queryTimer.getMinute() : queryTimer.getMinute()));
+		timer.setMonth(StringUtils.isBlank(queryTimer.getMonth()) ? "*" : (queryTimer.isEveryMonth() ? "*/" + queryTimer.getMonth() : queryTimer.getMonth()));
+		timer.setSecond(StringUtils.isBlank(queryTimer.getSecond()) ? "*" : (queryTimer.isEverySecond() ? "*/" + queryTimer.getSecond() : queryTimer.getSecond()));
+		timer.setYear(StringUtils.isBlank(queryTimer.getYear()) ? "*" : queryTimer.getYear());
+		timer.setCode(toCronFormat(getScheduleExpression(timer)));
+		return timer;
+	}
+	private ScheduleExpression getScheduleExpression(TimerEntity timerEntity) {
+		ScheduleExpression expression = new ScheduleExpression();
+		expression.dayOfMonth(timerEntity.getDayOfMonth());
+		expression.dayOfWeek(timerEntity.getDayOfWeek());
+		expression.end(timerEntity.getEnd());
+		expression.hour(timerEntity.getHour());
+		expression.minute(timerEntity.getMinute());
+		expression.month(timerEntity.getMonth());
+		expression.second(timerEntity.getSecond());
+		expression.start(timerEntity.getStart());
+		expression.year(timerEntity.getYear());
+		return expression;
+	}
+	private String toCronFormat(ScheduleExpression schedule) {
+		return schedule.getSecond() + " " +
+				schedule.getMinute() + " " +
+				schedule.getHour() + " " +
+				schedule.getDayOfMonth() + " " +
+				schedule.getMonth() + " " +
+				schedule.getDayOfWeek() + " " +
+				schedule.getYear();
+	}
 	@Override
 	public List<QueryScheduler> list(Long offset, Long limit, String sort, String orderBy, String filter) {
 		return Collections.emptyList();

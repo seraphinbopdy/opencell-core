217ba5ae9c3b9c2ab2e20f50d149537d6766b0a8
Abdelmounaim Akadid <abdelmounaim.akadid@opencellsoft.com>
2025-06-13 10:39:30 +0000

    Revert "Bugfix/INTRD-31828 17X (pull request #13549)"

--- opencell-admin/ejbs/src/main/java/org/meveo/service/billing/impl/article/AccountingArticleService.java
+++ opencell-admin/ejbs/src/main/java/org/meveo/service/billing/impl/article/AccountingArticleService.java
@@ -7,7 +7,6 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collection;
 import java.util.Collections;
-import java.util.Comparator;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -141,19 +140,17 @@ public class AccountingArticleService extends BusinessService<AccountingArticle>
                     && filterMappingLines(walletOperation, articleMappingLine.getMappingKeyEL())) {
 
                 filteredArticleMappingLines.add(articleMappingLine);
-			}
+		}
 		}
 		
 		AttributeMappingLineMatch attributeMappingLineMatch = new AttributeMappingLineMatch();
-		List<ArticleMappingLine> tmpArticleMappingLines = new ArrayList<>(filteredArticleMappingLines);
-		filteredArticleMappingLines.sort(Comparator.comparing(alm -> alm.getAttributesMapping().isEmpty()));
 		for (ArticleMappingLine aml : filteredArticleMappingLines) {
-			var noneAttributeExist = tmpArticleMappingLines.stream().allMatch(amls -> amls.getAttributesMapping().isEmpty());
-			if (aml.getAttributesMapping().isEmpty() && noneAttributeExist) {
+			
+			if (aml.getAttributesMapping().isEmpty()) {
                 attributeMappingLineMatch.addFullMatch(aml);
                 continue;
-			}
-   
+		}
+            
 			List<AttributeMapping> matchedAttributesMapping = new ArrayList<>();
 			List<AttributeMapping> attributesMapping = aml.getAttributesMapping();
 				AtomicBoolean continueProcess = new AtomicBoolean(true);
@@ -162,7 +159,6 @@ public class AccountingArticleService extends BusinessService<AccountingArticle>
 						if (continueProcess.get()) {
 							if (checkAttribute(product, walletOperation, attributes, attributeMapping)) {
 								matchedAttributesMapping.add(attributeMapping);
-								tmpArticleMappingLines.add(aml);
 							} else {
 								// for AND operator, if at least we have 1 unmatchedAttributs (else), all previous matchedAttribut shall not taken into account
 								matchedAttributesMapping.clear();
